6f70e6b7182290b4108830de29bfe401675a991a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-26 07:27:23 +0330

    add topbar component to app module

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import TopBar from "./components/topbar/TopBar";
+
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
 }
 
 export default App;
