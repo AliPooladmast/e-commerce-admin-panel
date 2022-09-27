6eea92c9f3d7e826d4329b252ed9ac200ac3c13b
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-27 07:42:44 +0330

    add menu items to sidebar module

--- src/components/siderbar/SideBar.jsx
+++ src/components/siderbar/SideBar.jsx
@@ -1,7 +1,30 @@
+import { LineStyle, Timeline, TrendingUp } from "@material-ui/icons";
 import style from "./SideBar.module.scss";
 
 const SideBar = () => {
-  return <div className={style.SideBar}>SideBar</div>;
+  return (
+    <div className={style.SideBar}>
+      <div className={style.Wrapper}>
+        <div className={style.Menu}>
+          <h1>Dashboard</h1>
+          <ul>
+            <li>
+              <LineStyle />
+              Home
+            </li>
+            <li>
+              <Timeline />
+              Analytics
+            </li>
+            <li>
+              <TrendingUp />
+              Sales
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
