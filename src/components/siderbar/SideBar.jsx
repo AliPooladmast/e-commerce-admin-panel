05fed06dff24151c49dc1b6e91b40d77ca907056
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-27 09:41:37 +0330

    add more menu section to sidebar

--- src/components/siderbar/SideBar.jsx
+++ src/components/siderbar/SideBar.jsx
@@ -22,6 +22,61 @@ const SideBar = () => {
             </li>
           </ul>
         </div>
+        <div className={style.Menu}>
+          <h1>Quick Menu</h1>
+          <ul>
+            <li>
+              <LineStyle className={style.Icons} />
+              Users
+            </li>
+            <li>
+              <Timeline className={style.Icons} />
+              Products
+            </li>
+            <li>
+              <TrendingUp className={style.Icons} />
+              Transactions
+            </li>
+            <li>
+              <TrendingUp className={style.Icons} />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className={style.Menu}>
+          <h1>Notifications</h1>
+          <ul>
+            <li>
+              <LineStyle className={style.Icons} />
+              Mail
+            </li>
+            <li>
+              <Timeline className={style.Icons} />
+              Feedback
+            </li>
+            <li>
+              <TrendingUp className={style.Icons} />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className={style.Menu}>
+          <h1>Staff</h1>
+          <ul>
+            <li>
+              <LineStyle className={style.Icons} />
+              Manage
+            </li>
+            <li>
+              <Timeline className={style.Icons} />
+              Analytics
+            </li>
+            <li>
+              <TrendingUp className={style.Icons} />
+              Reports
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
