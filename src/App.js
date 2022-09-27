c1c2b21df9a51372121ff16c32c87e8cdb36de25
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-27 07:26:48 +0330

    import side bar to app module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import SideBar from "./components/siderbar/SideBar";
 import TopBar from "./components/topbar/TopBar";
+import style from "./App.module.scss";
 
 function App() {
   return (
     <div>
       <TopBar />
+      <div className={style.MainLayout}>
+        <SideBar />
+      </div>
     </div>
   );
 }
