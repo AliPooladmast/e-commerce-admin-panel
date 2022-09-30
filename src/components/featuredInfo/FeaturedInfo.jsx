11f2231f6d02399f48d5b43709702223e2b2ba97
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-30 17:16:23 +0330

    add more element as feature item to the layout

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,4 +1,4 @@
-import { ArrowDownward } from "@material-ui/icons";
+import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
 import style from "./featuredInfo.module.scss";
 
 const FeaturedInfo = () => {
@@ -11,6 +11,28 @@ const FeaturedInfo = () => {
           <span className={style.MoneyRate}>
             -13.2
             <ArrowDownward />
+          </span>
+        </div>
+        <span className={style.SubTitle}>Compared to last month</span>
+      </div>
+      <div className={style.Item}>
+        <span className={style.Title}>Sales</span>
+        <div className={style.MoneyContainer}>
+          <span className={style.Money}>$7.24</span>
+          <span className={style.MoneyRate}>
+            -11.8
+            <ArrowDownward />
+          </span>
+        </div>
+        <span className={style.SubTitle}>Compared to last month</span>
+      </div>
+      <div className={style.Item}>
+        <span className={style.Title}>Costs</span>
+        <div className={style.MoneyContainer}>
+          <span className={style.Money}>$2.67</span>
+          <span className={style.MoneyRate}>
+            +9.8
+            <ArrowUpward />
           </span>
         </div>
         <span className={style.SubTitle}>Compared to last month</span>
