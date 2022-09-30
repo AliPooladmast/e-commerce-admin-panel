7e51c6005111c585894b61f00fc911c6e37b213a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-09-30 17:05:55 +0330

    add content as item to featured info

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,7 +1,22 @@
+import { ArrowDownward } from "@material-ui/icons";
 import style from "./featuredInfo.module.scss";
 
 const FeaturedInfo = () => {
-  return <div className={style.FeaturedInfo}>FeaturedInfo</div>;
+  return (
+    <div className={style.FeaturedInfo}>
+      <div className={style.Item}>
+        <span className={style.Title}>Revenue</span>
+        <div className={style.MoneyContainer}>
+          <span className={style.Money}>$5.14</span>
+          <span className={style.MoneyRate}>
+            -13.2
+            <ArrowDownward />
+          </span>
+        </div>
+        <span className={style.SubTitle}>Compared to last month</span>
+      </div>
+    </div>
+  );
 };
 
 export default FeaturedInfo;
