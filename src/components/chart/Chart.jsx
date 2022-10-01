c3a68cdeefe7953b12f7c4e17419f1e42971fe8b
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-01 22:05:27 +0330

    add title for chart

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,7 +1,11 @@
 import style from "./chart.module.scss";
 
 const Chart = () => {
-  return <div className={style.Chart}>Chart</div>;
+  return (
+    <div className={style.Chart}>
+      <h1 className={style.Title}>Sales Analytics</h1>
+    </div>
+  );
 };
 
 export default Chart;
