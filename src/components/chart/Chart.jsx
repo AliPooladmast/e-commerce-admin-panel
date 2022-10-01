26c1f46750635165bb9c301a97621a6a6c191ab5
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-01 22:57:54 +0330

    replace chart module hard code data with props

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -8,67 +8,16 @@ import {
 } from "recharts";
 import style from "./chart.module.scss";
 
-const Chart = () => {
-  const data = [
-    {
-      name: "Jan",
-      "active user": 4000,
-    },
-    {
-      name: "Feb",
-      "active user": 3000,
-    },
-    {
-      name: "Mar",
-      "active user": 2000,
-    },
-    {
-      name: "Apr",
-      "active user": 4000,
-    },
-    {
-      name: "May",
-      "active user": 3000,
-    },
-    {
-      name: "Jun",
-      "active user": 1000,
-    },
-    {
-      name: "Jul",
-      "active user": 3000,
-    },
-    {
-      name: "Aug",
-      "active user": 2000,
-    },
-    {
-      name: "Sep",
-      "active user": 4000,
-    },
-    {
-      name: "Oct",
-      "active user": 3000,
-    },
-    {
-      name: "Nov",
-      "active user": 4000,
-    },
-    {
-      name: "Dec",
-      "active user": 2000,
-    },
-  ];
-
+const Chart = ({ title, data, dataKey, grid }) => {
   return (
     <div className={style.Chart}>
-      <h1 className={style.Title}>Active Users</h1>
+      <h1 className={style.Title}>{title}</h1>
       <ResponsiveContainer width="100%" aspect={4 / 1}>
         <LineChart data={data}>
           <XAxis dataKey={"name"} stroke="#5550bd" />
-          <Line dataKey={"active user"} type={"monotone"} stroke="#5550bd" />
+          <Line dataKey={dataKey} type={"monotone"} stroke="#5550bd" />
           <Tooltip />
-          <CartesianGrid stroke="#e7e5e5" strokeDasharray="5 5" />
+          {grid && <CartesianGrid stroke="#e7e5e5" strokeDasharray="5 5" />}
         </LineChart>
       </ResponsiveContainer>
     </div>
