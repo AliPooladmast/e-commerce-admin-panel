a25953f2dfc77cbbc1a6c88d5f809ab7fb5b728a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-01 22:58:30 +0330

    create dummy data file and insert hard code data for user chart

---
+++ src/DummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "active user": 4000,
+  },
+  {
+    name: "Feb",
+    "active user": 3000,
+  },
+  {
+    name: "Mar",
+    "active user": 2000,
+  },
+  {
+    name: "Apr",
+    "active user": 4000,
+  },
+  {
+    name: "May",
+    "active user": 3000,
+  },
+  {
+    name: "Jun",
+    "active user": 1000,
+  },
+  {
+    name: "Jul",
+    "active user": 3000,
+  },
+  {
+    name: "Aug",
+    "active user": 2000,
+  },
+  {
+    name: "Sep",
+    "active user": 4000,
+  },
+  {
+    name: "Oct",
+    "active user": 3000,
+  },
+  {
+    name: "Nov",
+    "active user": 4000,
+  },
+  {
+    name: "Dec",
+    "active user": 2000,
+  },
+];
