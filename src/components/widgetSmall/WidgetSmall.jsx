0f066f61be83188c276bc9f097bfee3a81f4cf38
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-02 22:12:03 +0330

    add and align more user items

--- src/components/widgetSmall/WidgetSmall.jsx
+++ src/components/widgetSmall/WidgetSmall.jsx
@@ -19,6 +19,58 @@ const WidgetSmall = () => {
             <Visibility /> Display
           </button>
         </li>
+        <li className={style.ListItem}>
+          <img
+            src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg"
+            alt="profile"
+          />
+          <div className={style.Us}>
+            <span className={style.UserName}>Mike Ford</span>
+            <span className={style.UserTitle}>Web Developer</span>
+          </div>
+          <button>
+            <Visibility /> Display
+          </button>
+        </li>
+        <li className={style.ListItem}>
+          <img
+            src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg"
+            alt="profile"
+          />
+          <div className={style.Us}>
+            <span className={style.UserName}>Mike Ford</span>
+            <span className={style.UserTitle}>Web Developer</span>
+          </div>
+          <button>
+            <Visibility /> Display
+          </button>
+        </li>
+        <li className={style.ListItem}>
+          <img
+            src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg"
+            alt="profile"
+          />
+          <div className={style.Us}>
+            <span className={style.UserName}>Mike Ford</span>
+            <span className={style.UserTitle}>Web Developer</span>
+          </div>
+          <button>
+            <Visibility /> Display
+          </button>
+        </li>
+        <li className={style.ListItem}>
+          <img
+            src="https://d2qp0siotla746.cloudfront.net/img/use-cases/profile-picture/template_3.jpg"
+            alt="profile"
+          />
+          <div className={style.Us}>
+            <span className={style.UserName}>Mike Ford</span>
+            <span className={style.UserTitle}>Web Developer</span>
+          </div>
+          <button>
+            <Visibility /> Display
+          </button>
+        </li>
       </ul>
     </div>
   );
