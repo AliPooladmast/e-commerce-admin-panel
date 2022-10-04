8faea655d20aa7f69e93e34043d4e11bdaa4eed3
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-04 22:27:11 +0330

    Set routes for home and users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 import Users from "./pages/users/Users";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Users />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
