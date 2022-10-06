68fd87275bba1472d1f3348c75a4afb42b34265f
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-06 17:29:20 +0330

    replace user rows data to dummy data file

--- src/DummyData.js
+++ src/DummyData.js
@@ -48,3 +48,114 @@ export const userData = [
     "active user": 2000,
   },
 ];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 2,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 3,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 4,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 5,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 6,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 7,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 8,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 9,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 10,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 11,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 12,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+];
--- src/components/chart/userLists/UserLists.jsx
+++ src/components/chart/userLists/UserLists.jsx
@@ -3,117 +3,7 @@ import style from "./userLists.module.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { DeleteOutlined } from "@material-ui/icons";
 import { Box } from "@material-ui/core";
-
-const rows = [
-  {
-    id: 1,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 2,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 3,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 4,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 5,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 6,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 7,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 8,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 9,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 10,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 11,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-  {
-    id: 12,
-    username: "Mathew Perry",
-    avatar:
-      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
-    email: "mathew.perry@gmail.com",
-    status: "active",
-    transaction: "$320.00",
-  },
-];
+import { userRows } from "../../../DummyData";
 
 const columns = [
   { field: "id", headerName: "ID", width: 70 },
@@ -152,7 +42,7 @@ export default function UserLists() {
   return (
     <Box className={style.UserLists}>
       <DataGrid
-        rows={rows}
+        rows={userRows}
         columns={columns}
         pageSize={8}
         checkboxSelection
