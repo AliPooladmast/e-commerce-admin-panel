97133d4b839b22f572de481eee3019256cea03dc
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-06 17:45:04 +0330

    create handle for deleting rows

--- src/components/chart/userLists/UserLists.jsx
+++ src/components/chart/userLists/UserLists.jsx
@@ -1,51 +1,60 @@
-import * as React from "react";
 import style from "./userLists.module.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { DeleteOutlined } from "@material-ui/icons";
 import { Box } from "@material-ui/core";
 import { userRows } from "../../../DummyData";
 import { Link } from "react-router-dom";
-
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  {
-    field: "user",
-    headerName: "User",
-    width: 200,
-    renderCell: (params) => {
-      return (
-        <div className={style.User}>
-          <img src={params.row.avatar} alt="avatar" />
-          {params.row.username}
-        </div>
-      );
-    },
-  },
-  { field: "email", headerName: "Email", width: 250 },
-  { field: "status", headerName: "Status", width: 150 },
-  { field: "transaction", headerName: "Transaction Volume", width: 200 },
-  {
-    field: "action",
-    headerName: "Action",
-    width: 100,
-    renderCell: (params) => {
-      return (
-        <>
-          <Link to={"/users/" + params.row.id}>
-            <button className={style.Edit}>Edit</button>
-          </Link>
-          <DeleteOutlined className={style.Delete} />
-        </>
-      );
-    },
-  },
-];
+import { useState } from "react";
 
 export default function UserLists() {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <div className={style.User}>
+            <img src={params.row.avatar} alt="avatar" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 250 },
+    { field: "status", headerName: "Status", width: 150 },
+    { field: "transaction", headerName: "Transaction Volume", width: 200 },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 100,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={"/users/" + params.row.id}>
+              <button className={style.Edit}>Edit</button>
+            </Link>
+            <DeleteOutlined
+              className={style.Delete}
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
+
   return (
     <Box className={style.UserLists}>
       <DataGrid
-        rows={userRows}
+        rows={data}
         columns={columns}
         pageSize={8}
         checkboxSelection
