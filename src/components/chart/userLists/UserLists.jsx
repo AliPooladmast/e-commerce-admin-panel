042f61ef118e3fbe1481b731692c97b853c35298
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-06 16:03:42 +0330

    add typical data grid to user lists page

--- src/components/chart/userLists/UserLists.jsx
+++ src/components/chart/userLists/UserLists.jsx
@@ -1,7 +1,22 @@
+import * as React from "react";
 import style from "./userLists.module.scss";
+import { DataGrid } from "@mui/x-data-grid";
 
-const UserLists = () => {
-  return <div className={style.UserLists}>UserLists</div>;
-};
+const rows = [
+  { id: 1, col1: "Hello", col2: "World" },
+  { id: 2, col1: "DataGridPro", col2: "is Awesome" },
+  { id: 3, col1: "MUI", col2: "is Amazing" },
+];
 
-export default UserLists;
+const columns = [
+  { field: "col1", headerName: "Column 1", width: 150 },
+  { field: "col2", headerName: "Column 2", width: 150 },
+];
+
+export default function UserLists() {
+  return (
+    <div className={style.UserLists}>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
+  );
+}
