b97e3f34085c7050c56731556e196fb07301944f
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-06 16:24:28 +0330

    add custom rows and columns

--- src/components/chart/userLists/UserLists.jsx
+++ src/components/chart/userLists/UserLists.jsx
@@ -3,14 +3,59 @@ import style from "./userLists.module.scss";
 import { DataGrid } from "@mui/x-data-grid";
 
 const rows = [
-  { id: 1, col1: "Hello", col2: "World" },
-  { id: 2, col1: "DataGridPro", col2: "is Awesome" },
-  { id: 3, col1: "MUI", col2: "is Amazing" },
+  {
+    id: 1,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 2,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 3,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 4,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
+  {
+    id: 5,
+    username: "Mathew Perry",
+    avatar:
+      "https://www.stuttgarter-nachrichten.de/media.media.0341ffdd-892c-4752-99b3-7ef350d6780f.original1024.jpg",
+    email: "mathew.perry@gmail.com",
+    status: "active",
+    transaction: "$320.00",
+  },
 ];
 
 const columns = [
-  { field: "col1", headerName: "Column 1", width: 150 },
-  { field: "col2", headerName: "Column 2", width: 150 },
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "username", headerName: "Username", width: 200 },
+  { field: "email", headerName: "Email", width: 250 },
+  { field: "status", headerName: "Status", width: 150 },
+  { field: "transaction", headerName: "Transaction Volume", width: 200 },
 ];
 
 export default function UserLists() {
