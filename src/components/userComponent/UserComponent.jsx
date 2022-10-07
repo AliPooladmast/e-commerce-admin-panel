7c912b8bde6a7260e35b352e429e7cef11d85b1a
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-07 15:16:57 +0330

    add top section in user show box

--- src/components/userComponent/UserComponent.jsx
+++ src/components/userComponent/UserComponent.jsx
@@ -8,7 +8,18 @@ const UserComponent = () => {
         <button>Create</button>
       </div>
       <div className={style.UserContainer}>
-        <div className={style.Show}>Show</div>
+        <div className={style.Show}>
+          <div className={style.Top}>
+            <img
+              src="https://t4.ftcdn.net/jpg/01/87/14/51/360_F_187145146_SB34n4kdiNqlVSvaTy4YUJcUWjNO540N.jpg"
+              alt="user profile"
+            />
+            <div className={style.TitleContainer}>
+              <span className={style.Name}>Emily Dimmer</span>
+              <span className={style.Title}>IT Specialist</span>
+            </div>
+          </div>
+        </div>
         <div className={style.Update}>Update</div>
       </div>
     </div>
