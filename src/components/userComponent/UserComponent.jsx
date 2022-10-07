8fa9d566804da53d3f83db9e4fce3b4a3d42047e
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-07 11:39:59 +0330

    add title container for user component

--- src/components/userComponent/UserComponent.jsx
+++ src/components/userComponent/UserComponent.jsx
@@ -1,7 +1,14 @@
 import style from "./userComponent.module.scss";
 
 const UserComponent = () => {
-  return <div className={style.UserComponent}>UserComponent</div>;
+  return (
+    <div className={style.UserComponent}>
+      <div className={style.TitleContainer}>
+        <h1>Edit User</h1>
+        <button>Create</button>
+      </div>
+    </div>
+  );
 };
 
 export default UserComponent;
