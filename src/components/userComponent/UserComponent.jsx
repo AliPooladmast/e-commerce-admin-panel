c27ede28b6cee99a27fa19bbf447a4fb9e904b48
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-07 18:36:25 +0330

    add link for create new user

--- src/components/userComponent/UserComponent.jsx
+++ src/components/userComponent/UserComponent.jsx
@@ -6,6 +6,7 @@ import {
   PhoneAndroid,
   Publish,
 } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 import style from "./userComponent.module.scss";
 
 const UserComponent = () => {
@@ -13,7 +14,9 @@ const UserComponent = () => {
     <div className={style.UserComponent}>
       <div className={style.TitleContainer}>
         <h1>Edit User</h1>
-        <button>Create</button>
+        <Link to={"/newUser"}>
+          <button>Create</button>
+        </Link>
       </div>
       <div className={style.UserContainer}>
         <div className={style.Show}>
