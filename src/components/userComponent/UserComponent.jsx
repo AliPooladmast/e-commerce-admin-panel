b954bf9d0e87c382295b7d8f4776f564e50ca585
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-07 16:39:21 +0330

    add bottom section as user details in user show box

--- src/components/userComponent/UserComponent.jsx
+++ src/components/userComponent/UserComponent.jsx
@@ -1,3 +1,10 @@
+import {
+  CalendarToday,
+  LocationSearching,
+  MailOutline,
+  PermIdentity,
+  PhoneAndroid,
+} from "@material-ui/icons";
 import style from "./userComponent.module.scss";
 
 const UserComponent = () => {
@@ -17,6 +24,30 @@ const UserComponent = () => {
             <div className={style.TitleContainer}>
               <span className={style.Name}>Emily Dimmer</span>
               <span className={style.Title}>IT Specialist</span>
+            </div>
+          </div>
+          <div className={style.Bottom}>
+            <span className={style.Title}>Account Details</span>
+            <div className={style.Info}>
+              <PermIdentity className={style.Icon} />
+              <span>Emily Dimmer</span>
+            </div>
+            <div className={style.Info}>
+              <CalendarToday className={style.Icon} />
+              <span>14.06.1995</span>
+            </div>
+            <span className={style.Title}>Contact Details</span>
+            <div className={style.Info}>
+              <PhoneAndroid className={style.Icon} />
+              <span>+25 253 254 24</span>
+            </div>
+            <div className={style.Info}>
+              <MailOutline className={style.Icon} />
+              <span>emily.dimmer@gmail.com</span>
+            </div>
+            <div className={style.Info}>
+              <LocationSearching className={style.Icon} />
+              <span>Berlin | Germany</span>
             </div>
           </div>
         </div>
