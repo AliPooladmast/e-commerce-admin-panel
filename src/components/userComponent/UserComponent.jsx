02331dcd6f04486087ffde1fc300d973c33ad388
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-07 11:30:08 +0330

    create user component module

---
+++ src/components/userComponent/UserComponent.jsx
@@ -0,0 +1,7 @@
+import style from "./userComponent.module.scss";
+
+const UserComponent = () => {
+  return <div className={style.UserComponent}>UserComponent</div>;
+};
+
+export default UserComponent;
