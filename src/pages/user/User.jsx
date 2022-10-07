2020bd3fb93a79201b12533e466102f16f077d4d
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-07 11:30:46 +0330

    create user page

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,7 +1,20 @@
+import SideBar from "../../components/siderbar/SideBar";
+import TopBar from "../../components/topbar/TopBar";
+import UserComponent from "../../components/userComponent/UserComponent";
 import style from "./user.module.scss";
 
 const User = () => {
-  return <div className={style.User}>User</div>;
+  return (
+    <>
+      <TopBar />
+      <div className={style.MainLayout}>
+        <SideBar />
+        <div className={style.Body}>
+          <UserComponent />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default User;
