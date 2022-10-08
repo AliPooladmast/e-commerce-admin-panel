b54c39cd1b1b5946209d0dd04b464d06b76959a9
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-08 23:20:56 +0330

    create html structure content for new user component

--- src/components/newUserComponent/NewUserComponent.jsx
+++ src/components/newUserComponent/NewUserComponent.jsx
@@ -1,7 +1,63 @@
 import style from "./newUserComponent.module.scss";
 
 const NewUserComponent = () => {
-  return <div className={style.NewUserComponent}>NewUserComponent</div>;
+  return (
+    <div className={style.NewUserComponent}>
+      <h1>New User</h1>
+
+      <form>
+        <div className={style.Item}>
+          <label>Username</label>
+          <input type="text" placeholder="mike" />
+        </div>
+
+        <div className={style.Item}>
+          <label>Full Name</label>
+          <input type="text" placeholder="Mike Dormant" />
+        </div>
+
+        <div className={style.Item}>
+          <label>Email</label>
+          <input type="email" placeholder="mike.dormant@gmail.com" />
+        </div>
+
+        <div className={style.Item}>
+          <label>Password</label>
+          <input type="password" placeholder="password" />
+        </div>
+
+        <div className={style.Item}>
+          <label>Phone</label>
+          <input type="text" placeholder="+15 254 356 98" />
+        </div>
+
+        <div className={style.Item}>
+          <label>Address</label>
+          <input type="text" placeholder="Calgary | Canada" />
+        </div>
+
+        <div className={style.Item}>
+          <label>Gender</label>
+          <div className={style.Gender}>
+            <label for="male">Male</label>
+            <input type="radio" id="male" value="male" />
+            <label for="female">Female</label>
+            <input type="radio" id="female" value="female" />
+            <label for="other">Other</label>
+            <input type="radio" id="other" value="other" />
+          </div>
+        </div>
+
+        <div className={style.Item}>
+          <label>Active</label>
+          <select name="active" id="active">
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+          </select>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default NewUserComponent;
