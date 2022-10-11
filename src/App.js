3a77f8eb56b04d0ef0e47c580768a36f4d41ccce
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-11 22:54:55 +0330

    add routes for porducts pages

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import Home from "./pages/home/Home";
 import Users from "./pages/users/Users";
 import User from "./pages/user/User";
 import NewUser from "./pages/newuser/NewUser";
+import Products from "./pages/products/Products";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/users" element={<Users />} />
         <Route path="/newuser" element={<NewUser />} />
         <Route path="/user" element={<User />}>
+          <Route path=":id" element={<User />} />
+        </Route>
+
+        <Route path="/products" element={<Products />} />
+        <Route path="/newproduct" element={<NewUser />} />
+        <Route path="/product" element={<User />}>
           <Route path=":id" element={<User />} />
         </Route>
       </Routes>
