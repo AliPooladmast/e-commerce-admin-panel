0ae4ee375b6889ff91a08ee7b08367bd29b97437
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-12 23:14:02 +0330

    add title section to product page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,7 +1,14 @@
 import style from "./product.module.scss";
 
 const Product = () => {
-  return <div className={style.Product}>Product</div>;
+  return (
+    <div className={style.Product}>
+      <div className={style.TitleContainer}>
+        <h1>Product</h1>
+        <button>Create</button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
