9941b863ebf85cd5ee29d5ad26c857a6c08fea20
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-15 11:21:21 +0330

    create login api call

---
+++ src/redux/apiCalls.js
@@ -0,0 +1,12 @@
+import { userRequest } from "../requestMethods";
+import { loginFailure, loginStart, loginSuccess } from "./userSlice";
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart());
+  try {
+    const res = await userRequest.post("/auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
