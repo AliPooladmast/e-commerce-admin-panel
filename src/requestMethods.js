fed80312d86e8f0cc00262cf98ba1b2f3fbf297f
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-15 14:26:43 +0330

    catch user token from local storage for later api calls

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMDIxYTdkZThlNjdmMWU4NmE3MWY0YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzQyOTQwMCwiZXhwIjoxNjYzNjg4NjAwfQ.1SuvPQ-X0cCv8qE6GErVGmel2nndjvsmgNMn_4GOYw0";
+const TOKEN = localStorage.getItem("token");
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
