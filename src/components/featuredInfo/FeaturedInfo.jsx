7cee46cd5ae4fcacf43336e9fa47e9f307c638d8
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-16 13:12:47 +0330

    get incomes and attach data to home revenue card

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -1,16 +1,44 @@
 import { ArrowDownward, ArrowUpward } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import { userRequest } from "../../requestMethods";
 import style from "./featuredInfo.module.scss";
 
 const FeaturedInfo = () => {
+  const [income, setIncome] = useState([]);
+  const [monthChange, setMonthChange] = useState(0);
+
+  useEffect(() => {
+    const getIncome = async () => {
+      try {
+        const { data } = await userRequest.get("/orders/income");
+        data.sort((a, b) => a._id - b._id);
+        setIncome(data);
+        setMonthChange(
+          (data[data.length - 1].total / data[data.length - 2].total) * 100 -
+            100
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getIncome();
+  }, []);
+
   return (
     <div className={style.FeaturedInfo}>
       <div className={style.Item}>
         <span className={style.Title}>Revenue</span>
         <div className={style.MoneyContainer}>
-          <span className={style.Money}>$5.14</span>
+          <span className={style.Money}>
+            ${income[income.length - 1]?.total}
+          </span>
           <span className={style.MoneyRate}>
-            -13.2
-            <ArrowDownward className={style["Icon--Negetive"]} />
+            % {Math.floor(monthChange)}
+            {monthChange > 0 ? (
+              <ArrowUpward className={style.Icon} />
+            ) : (
+              <ArrowDownward className={style["Icon--Negetive"]} />
+            )}
           </span>
         </div>
         <span className={style.SubTitle}>Compared to last month</span>
