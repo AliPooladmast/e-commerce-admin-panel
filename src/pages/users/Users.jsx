82c9c8b920b4b6455ed7f8fc50c3b3a3dc2cd7fd
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-23 09:02:21 +0330

    set data grid according to users gotten from api call

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -3,34 +3,46 @@ import { DataGrid } from "@mui/x-data-grid";
 import { DeleteOutlined } from "@material-ui/icons";
 import { Box } from "@material-ui/core";
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { userRows } from "../../DummyData";
+import { useEffect } from "react";
+import { getUsers } from "../../redux/apiCalls";
+import { useDispatch, useSelector } from "react-redux";
+import noAvatar from "../../assets/icons/no-avatar.svg";
 
 export default function Users() {
-  const [data, setData] = useState(userRows);
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.user.users);
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
+  const handleDelete = (id) => {};
+
+  useEffect(() => {
+    getUsers(dispatch);
+  }, [dispatch]);
 
   const columns = [
-    { field: "id", headerName: "ID", width: 70 },
+    { field: "_id", headerName: "ID", width: 270 },
     {
       field: "user",
       headerName: "User",
-      width: 200,
+      width: 220,
       renderCell: (params) => {
         return (
           <div className={style.User}>
-            <img src={params.row.avatar} alt="avatar" />
+            <img src={params.row.img || noAvatar} alt="avatar" />
             {params.row.username}
           </div>
         );
       },
     },
-    { field: "email", headerName: "Email", width: 250 },
-    { field: "status", headerName: "Status", width: 150 },
-    { field: "transaction", headerName: "Transaction Volume", width: 200 },
+    { field: "email", headerName: "Email", width: 270 },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 100,
+      renderCell: (params) => {
+        return <span>{params.row.isAdmin ? "admin" : "user"}</span>;
+      },
+    },
+    // { field: "transaction", headerName: "Transaction Volume", width: 200 },
     {
       field: "action",
       headerName: "Action",
@@ -38,7 +50,7 @@ export default function Users() {
       renderCell: (params) => {
         return (
           <>
-            <Link to={"/user/" + params.row.id}>
+            <Link to={"/user/" + params.row._id}>
               <button className={style.Edit}>Edit</button>
             </Link>
             <DeleteOutlined
@@ -54,12 +66,13 @@ export default function Users() {
   return (
     <Box className={style.UserLists}>
       <DataGrid
-        rows={data}
+        rows={users}
         columns={columns}
         pageSize={8}
         rowsPerPageOptions={[8]}
         checkboxSelection
         disableSelectionOnClick
+        getRowId={(row) => row._id}
       />
     </Box>
   );
