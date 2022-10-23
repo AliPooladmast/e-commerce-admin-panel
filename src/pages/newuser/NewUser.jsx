d074dfd867b924f411c090188950b59cacdf37b5
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-23 11:20:23 +0330

    improve texts and fields according to db schemas

--- src/pages/newuser/NewUser.jsx
+++ src/pages/newuser/NewUser.jsx
@@ -8,53 +8,54 @@ const NewUser = () => {
       <form>
         <div className={style.Item}>
           <label>Username</label>
-          <input type="text" placeholder="mike" />
+          <input type="text" placeholder="enter username" />
         </div>
 
         <div className={style.Item}>
           <label>Full Name</label>
-          <input type="text" placeholder="Mike Dormant" />
+          <input type="text" placeholder="enter full name" />
         </div>
 
         <div className={style.Item}>
           <label>Email</label>
-          <input type="email" placeholder="mike.dormant@gmail.com" />
+          <input type="email" placeholder="enter email" />
         </div>
 
         <div className={style.Item}>
           <label>Password</label>
-          <input type="password" placeholder="password" />
+          <input type="password" placeholder="enter password" />
         </div>
 
         <div className={style.Item}>
           <label>Phone</label>
-          <input type="text" placeholder="+15 254 356 98" />
+          <input type="text" placeholder="enter phone number" />
         </div>
 
         <div className={style.Item}>
           <label>Address</label>
-          <input type="text" placeholder="Calgary | Canada" />
+          <input type="text" placeholder="enter address" />
         </div>
 
         <div className={style.Item}>
-          <label>Gender</label>
-          <div className={style.Gender}>
-            <label forhtml="male">Male</label>
-            <input type="radio" id="male" value="male" />
-            <label forhtml="female">Female</label>
-            <input type="radio" id="female" value="female" />
-            <label forhtml="other">Other</label>
-            <input type="radio" id="other" value="other" />
-          </div>
-        </div>
-
-        <div className={style.Item}>
-          <label>Active</label>
+          <label>Admin</label>
           <select name="active" id="active">
             <option value="yes">Yes</option>
             <option value="no">No</option>
           </select>
         </div>
+
+        <div className={style.Item}>
+          <label>Gender</label>
+          <div className={style.Gender}>
+            <input name="gender" type="radio" id="male" value="male" />
+            <label forhtml="male">Male</label>
+            <input name="gender" type="radio" id="female" value="female" />
+            <label forhtml="female">Female</label>
+            <input name="gender" type="radio" id="other" value="other" />
+            <label forhtml="other">Other</label>
+          </div>
+        </div>
+
         <div className={style.Item}>
           <button>Create</button>
         </div>
