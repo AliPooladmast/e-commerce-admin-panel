076e6126f6097a6867b1fdf10c9b0a30111d0006
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-10-23 14:51:35 +0330

    create edit user success reducer in user redux slice

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -26,9 +26,20 @@ const userSlice = createSlice({
       state.isFetching = false;
       state.users = action.payload;
     },
+    editUserSuccess: (state, action) => {
+      state.isFetching = false;
+      state.users[
+        state.users.findIndex((user) => user._id === action.payload._id)
+      ] = action.payload;
+    },
   },
 });
 
-export const { userStart, loginSuccess, userFailure, getUserSuccess } =
-  userSlice.actions;
+export const {
+  userStart,
+  loginSuccess,
+  userFailure,
+  getUserSuccess,
+  editUserSuccess,
+} = userSlice.actions;
 export default userSlice.reducer;
