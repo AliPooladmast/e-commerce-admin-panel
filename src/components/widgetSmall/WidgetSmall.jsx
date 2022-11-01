77c138f3785dd9c78aec032493d053ee22a27c51
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-01 13:14:51 +0330

    add scroll feature to widgets

--- src/components/widgetSmall/WidgetSmall.jsx
+++ src/components/widgetSmall/WidgetSmall.jsx
@@ -21,21 +21,23 @@ const WidgetSmall = () => {
 
   return (
     <div className={style.WidgetSmall}>
-      <span className={style.Title}>New Join Members</span>
-      <ul className={style.List}>
-        {users?.map((user) => (
-          <li className={style.ListItem} key={user._id}>
-            <img src={user.img || noAvatar} alt="profile" />
-            <div className={style.User}>
-              <span className={style.UserName}>{user.username}</span>
-              <span className={style.UserTitle}>{user.title || ""}</span>
-            </div>
-            <button>
-              <Visibility className={style.Icon} /> Display
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className={style.Wrapper}>
+        <div className={style.Title}>New Join Members</div>
+        <ul className={style.List}>
+          {users?.map((user) => (
+            <li className={style.ListItem} key={user._id}>
+              <img src={user.img || noAvatar} alt="profile" />
+              <div className={style.User}>
+                <span className={style.UserName}>{user.username}</span>
+                <span className={style.UserTitle}>{user.title || ""}</span>
+              </div>
+              <button>
+                <Visibility className={style.Icon} /> Display
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
