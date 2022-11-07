389d4fa957ec176935250752ac4f6eb4e478ecca
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-07 22:34:14 +0330

    add loading skeleton to feature card

--- src/components/featureCard/FeatureCard.jsx
+++ src/components/featureCard/FeatureCard.jsx
@@ -1,23 +1,36 @@
 import { ArrowDownward, ArrowUpward } from "@mui/icons-material";
+import { Skeleton } from "@mui/material";
 import React from "react";
 import style from "./featureCard.module.scss";
 
-const FeatureCard = ({ amount, percentage, title }) => {
+const FeatureCard = ({ amount, percentage, title, loading }) => {
   return (
     <div className={style.FeatureCard}>
-      <span className={style.Title}>{title}</span>
-      <div className={style.MoneyContainer}>
-        <span className={style.Money}>${amount}</span>
-        <span className={style.MoneyRate}>
-          % {percentage}
-          {percentage > 0 ? (
-            <ArrowUpward className={style.Icon} />
-          ) : (
-            <ArrowDownward className={style["Icon--Negetive"]} />
-          )}
-        </span>
-      </div>
-      <span className={style.SubTitle}>Compared to last month</span>
+      {loading ? (
+        <>
+          <Skeleton sx={{ fontSize: "20px" }} />
+          <div className={style.MoneyContainer}>
+            <Skeleton sx={{ fontSize: "30px" }} width="100%" />
+          </div>
+          <Skeleton />
+        </>
+      ) : (
+        <>
+          <span className={style.Title}>{title}</span>
+          <div className={style.MoneyContainer}>
+            <span className={style.Money}>${amount}</span>
+            <span className={style.MoneyRate}>
+              % {percentage}
+              {percentage > 0 ? (
+                <ArrowUpward className={style.Icon} />
+              ) : (
+                <ArrowDownward className={style["Icon--Negetive"]} />
+              )}
+            </span>
+          </div>
+          <span className={style.SubTitle}>Compared to last month</span>
+        </>
+      )}
     </div>
   );
 };
