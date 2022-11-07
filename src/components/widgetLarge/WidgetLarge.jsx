6724daa51deb5c9a488638c8793a5777953729ff
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-07 23:46:19 +0330

    add skeleton loading to large widget

--- src/components/widgetLarge/WidgetLarge.jsx
+++ src/components/widgetLarge/WidgetLarge.jsx
@@ -3,6 +3,7 @@ import { userRequest } from "../../requestMethods";
 import style from "./widgetLarge.module.scss";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
+import { Skeleton } from "@mui/material";
 TimeAgo.addLocale(en);
 const timeAgo = new TimeAgo("en-US");
 
@@ -26,31 +27,44 @@ const WidgetLarge = ({ users }) => {
       <div className={style.Wrapper}>
         <h1>Latest Transactions</h1>
         <table className={style.Table}>
-          <tbody>
-            <tr className={style.Row}>
-              <th className={style.Header}>Customer</th>
-              <th className={style.Header}>Date</th>
-              <th className={style.Header}>Amount</th>
-              <th className={style.Header}>Status</th>
-            </tr>
-            {orders.map((order) => (
-              <tr className={style.Row} key={order._id}>
-                <td className={style.User}>
-                  <span>
-                    {users?.find((user) => user._id === order.userId)
-                      ?.username || "anonymous"}
-                  </span>
-                </td>
-                <td className={style.Date}>
-                  {timeAgo.format(new Date(order.createdAt))}
-                </td>
-                <td className={style.Amount}>${order.amount}</td>
-                <td className={style[`Status--${order.status}`]}>
-                  <button>{order.status}</button>
-                </td>
+          {!orders?.length > 0 ? (
+            Array(6)
+              .fill(null)
+              .map((element) => (
+                <Skeleton
+                  style={{
+                    marginBottom: "20px",
+                  }}
+                  animation="wave"
+                />
+              ))
+          ) : (
+            <tbody>
+              <tr className={style.Row}>
+                <th className={style.Header}>Customer</th>
+                <th className={style.Header}>Date</th>
+                <th className={style.Header}>Amount</th>
+                <th className={style.Header}>Status</th>
               </tr>
-            ))}
-          </tbody>
+              {orders.map((order) => (
+                <tr className={style.Row} key={order._id}>
+                  <td className={style.User}>
+                    <span>
+                      {users?.find((user) => user._id === order.userId)
+                        ?.username || "anonymous"}
+                    </span>
+                  </td>
+                  <td className={style.Date}>
+                    {timeAgo.format(new Date(order.createdAt))}
+                  </td>
+                  <td className={style.Amount}>${order.amount}</td>
+                  <td className={style[`Status--${order.status}`]}>
+                    <button>{order.status}</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
         </table>
       </div>
     </div>
