fbe5333cdcc3e01110e561d51148ec1ad5f3bd5d
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-12 10:22:07 +0330

    create style slice in redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,6 +12,7 @@ import {
 import storage from "redux-persist/lib/storage";
 import userReducer from "./userSlice";
 import productSlice from "./productSlice";
+import styleSlice from "./styleSlice";
 
 const persistConfig = {
   key: "root",
@@ -22,6 +23,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   product: productSlice,
+  style: styleSlice,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
