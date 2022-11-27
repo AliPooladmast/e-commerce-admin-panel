b9250005ecdb5ca8c4e7acf60e2bdb5dd2d11b8e
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-11-27 08:56:27 +0330

    add message state and setMessage reducer

--- src/redux/uxSlice.js
+++ src/redux/uxSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   displaySideMenu: false,
+  message: { type: "info", text: null },
 };
 
 const styleSlice = createSlice({
@@ -11,8 +12,11 @@ const styleSlice = createSlice({
     sideMenuToggle: (state) => {
       state.displaySideMenu = !state.displaySideMenu;
     },
+    setMessage: (state, action) => {
+      state.message = action.payload;
+    },
   },
 });
 
-export const { sideMenuToggle } = styleSlice.actions;
+export const { sideMenuToggle, setMessage } = styleSlice.actions;
 export default styleSlice.reducer;
