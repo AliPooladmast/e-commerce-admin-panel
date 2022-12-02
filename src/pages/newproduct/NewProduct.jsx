853d84472c2840b102c6e9ee8f286a0725e98c7e
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-02 23:44:57 +0330

    fix image validation

--- src/pages/newproduct/NewProduct.jsx
+++ src/pages/newproduct/NewProduct.jsx
@@ -20,7 +20,7 @@ const Joi = require("joi");
 const schema = Joi.object({
   title: Joi.string().min(3).max(50).required().trim(),
   desc: Joi.string().min(3).max(1024),
-  img: Joi.string(),
+  img: Joi.string().min(0),
   categories: Joi.array().max(10).items(Joi.string().min(1).max(20)),
   size: Joi.array().required().max(10).items(Joi.string().min(1).max(5)),
   color: Joi.array().required().max(10).items(Joi.string().min(1).max(20)),
@@ -97,8 +97,8 @@ const NewProduct = () => {
 
   const handleCreate = (e) => {
     e.preventDefault();
-    const inputs = { ...input, ...multipleInput, color: colors };
-    const { error: joiError } = schema.validate(inputs);
+    const product = { ...input, ...multipleInput, color: colors, img: image };
+    const { error: joiError } = schema.validate(product);
 
     if (joiError) {
       dispatch(
@@ -108,7 +108,6 @@ const NewProduct = () => {
         })
       );
     } else {
-      const product = { ...inputs, img: image };
       addProduct(dispatch, product);
     }
   };
