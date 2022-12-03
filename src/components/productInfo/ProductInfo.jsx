09eba64c0dc9513d1bcf10789316f9c3e148eb34
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-03 12:43:18 +0330

    add color division for product info

--- src/components/productInfo/ProductInfo.jsx
+++ src/components/productInfo/ProductInfo.jsx
@@ -4,29 +4,47 @@ const ProductInfo = ({ product }) => {
   return (
     <div className={style.InfoContainer}>
       <div className={style.ImageContainer}>
-        <img src={product.img} alt="product view" />
-        <span>{product.title}</span>
+        <img src={product?.img} alt="product view" />
+        <span>{product?.title}</span>
       </div>
 
       <div className={style.Details}>
         <div className={style.Item}>
           <span className={style.Key}>Description:</span>
-          <span className={style.Value}>{product.desc}</span>
+          <span className={style.Value}>{product?.desc}</span>
         </div>
 
         <div className={style.Item}>
           <span className={style.Key}>Sales:</span>
-          <span className={style.Value}>{product.price}</span>
+          <span className={style.Value}>{product?.price}</span>
         </div>
 
         <div className={style.Item}>
           <span className={style.Key}>Categories:</span>
-          <span className={style.Value}>{product.categories?.join(", ")}</span>
+          <span className={style.Value}>{product?.categories?.join(", ")}</span>
+        </div>
+
+        <div className={style.Item}>
+          <span className={style.Key}>Sizes:</span>
+          <span className={style.Value}>{product?.size?.join(", ")}</span>
+        </div>
+
+        <div className={style.Item}>
+          <span className={style.Key}>Colors:</span>
+          <div className={style.ColorContainer}>
+            {product?.color?.map((color, index) => (
+              <div
+                key={index}
+                className={style.ColorCircle}
+                style={{ backgroundColor: color }}
+              ></div>
+            ))}
+          </div>
         </div>
 
         <div className={style.Item}>
           <span className={style.Key}>In stock:</span>
-          <span className={style.Value}>{product.inStock ? "Yes" : "No"}</span>
+          <span className={style.Value}>{product?.inStock}</span>
         </div>
       </div>
     </div>
