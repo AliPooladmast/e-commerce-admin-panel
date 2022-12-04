59d1de4faf2e88e557343057c0fb748d5b4d02f9
Ali Pooladmast <ali.pooladmast.gh@gmail.com>
2022-12-04 13:04:20 +0330

    fix small widget skeleton

--- src/components/widgetSmall/WidgetSmall.jsx
+++ src/components/widgetSmall/WidgetSmall.jsx
@@ -43,11 +43,19 @@ const WidgetSmall = ({ users, isFetching }) => {
                   >
                     <Skeleton
                       variant="circular"
-                      height={40}
-                      width={40}
-                      style={{ marginRight: "20px" }}
+                      style={{
+                        marginRight: "20px",
+                        minWidth: "40px",
+                        minHeight: "40px",
+                      }}
                     />
-                    <Skeleton width={200} height={20} animation="wave" />
+                    <Skeleton
+                      animation="wave"
+                      style={{
+                        flex: 1,
+                        minHeight: "20px",
+                      }}
+                    />
                   </div>
                 ))
             : users?.map((user) => (
